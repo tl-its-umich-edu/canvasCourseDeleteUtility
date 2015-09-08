d89ca252854e8dff66bfa63247763fa4747839f9
pushyami <pushyami@umich.edu>
2015-09-08 17:23:38 -0400

    TLUNIZIN-148 Creating a one jar file that has dependencies added to it

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # canvasCourseDeleteUtility
+
+ ## Build Directions
+ 1.CourseDeleteUtility$ `mvn clean install`
+ 2.Run `java -jar target/canvasCourseDeleteUtility.jar`
---
+++ src/main/java/edu/umich/tl/CourseDelete.java
@@ -0,0 +1,17 @@
+package edu.umich.tl;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+public class CourseDelete {
+	private static Log M_log = LogFactory.getLog(CourseDelete.class);
+
+	public static void main(String[] args) {
+		M_log.debug("main(): called");
+
+	}
+
+
+}
