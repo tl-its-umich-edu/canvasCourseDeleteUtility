fcc6c49940898d075efa468ffd3309bc2b1de216
pushyami <pushyami@umich.edu>
2015-10-01 11:14:11 -0400

    TLUNIZIN-148 getting the Terms to deleting the courses from properties file

--- src/main/java/edu/umich/tl/CourseDelete.java
+++ src/main/java/edu/umich/tl/CourseDelete.java
@@ -2,10 +2,13 @@ package edu.umich.tl;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -14,6 +17,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
 import org.apache.http.util.EntityUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -25,34 +31,42 @@ import edu.umich.tl.ApiCallHandler.RequestTypeEnum;
 
 
 public class CourseDelete {
-	private static final String SIS_TERM_ID = "sis.term.id";
+	private static final String TERM = "term";
+	private static final String TERM_COUNT = "term.count";
 	private static final String CANVAS_URL = "canvas.url";
 	private static final String CANVAS_TOKEN = "canvas.token";
 	private static final String ESB_URL = "esb.url";
 	private static final String API_CALL_TYPE = "api.call.type";
-	private static Log M_log = LogFactory.getLog(CourseDelete.class);
 	protected static String canvasURL = null;
 	protected static String canvasToken = null;
 	protected static String esbURL = null;
-	protected static String sisTermIdFromPropsFile = null;
+	protected static HashMap<String, DateTime> termsInfo = new HashMap<String, DateTime>();
 	//This parameter decide to make canvas or ESB api call
 	protected static CanvasCallEnum canvasCall;
+	
+	private static Log M_log = LogFactory.getLog(CourseDelete.class);
 
 	public static void main(String[] args) {
 		M_log.debug("main(): called");
 		readPropertiesFromFile(args);
 		ApiCallHandler apiHandler=new ApiCallHandler(canvasCall);
-		CoursesForDelete coursesForDelete=new CoursesForDelete();
-		getTerms(apiHandler,coursesForDelete);
-		String canvasTermIdForSisTermId = getCanvasTermIdForSisTermId(coursesForDelete);
-		if(canvasTermIdForSisTermId==null) {
-			M_log.error("Cannot find \"CanvasTermId\" for \"sisTermId = "+sisTermIdFromPropsFile+
-					"\" provided. Please correct the \"sis.term.id\" property provided from \"canvasCourseDelete.properties\" file.");
+		ArrayList<Term> terms = getTerms(apiHandler);
+		ArrayList<String> previousTerms = getPreviousTerms(terms);
+		if(previousTerms.isEmpty()) {
+			M_log.error("No previous terms could be determined for courses deletion");
 			System.exit(1);
 		}
-		getUnpublishedCourseList(canvasTermIdForSisTermId,coursesForDelete,apiHandler,null);
-		//Todo Delete
-		M_log.debug(coursesForDelete.getCourses().size());
+		/*
+		 * Their may be multiple term courses that needs to be deleted for a cron job at that point in time. The Unpublished course list can be huge so the 
+		 * design would be focusing on one term at a time and verifying for each course in a term if people exist, content added and activity happened for deleting the course. 
+		 *  
+		 */
+		for (String canvasTermId : previousTerms) {
+			CoursesForDelete coursesForDelete=new CoursesForDelete();
+			getUnpublishedCourseList(canvasTermId,coursesForDelete,apiHandler,null);
+			//Todo delete below line
+			M_log.debug("CourseList: "+coursesForDelete.getCourses().size()+" term: "+canvasTermId);
+		}
 	}
 
 	public enum CanvasCallEnum{
@@ -66,15 +80,44 @@ public class CourseDelete {
 		String propFileLocation;
 		if(args.length == 0) {
 			M_log.error("Command line arguments are not provided");
-			System.exit(0);
+			System.exit(1);
 		}
 		propFileLocation = args[0];
 		properties = Utils.getPropertiesFromURL(propFileLocation);
 		canvasToken = properties.getProperty(CANVAS_TOKEN);
 		canvasURL = properties.getProperty(CANVAS_URL);
 		esbURL=properties.getProperty(ESB_URL);
-		sisTermIdFromPropsFile = properties.getProperty(SIS_TERM_ID);
+		getTermsInfoFromProperties(properties);
 		checkForApiType(properties);
+	}
+	/*
+	 * The Terms that needs for course deletion are listed in the properties file. Each term has 2 piece of information 
+	 * term1=2030;04/06/15, the sisTermCode and the endDate of that term. We will check if the current system date is
+	 * past the endDate then that term becomes eligible for deleting the unused/unpublished courses that are created 
+	 * as apart of auto-provisioning of courses in canvas
+	 * 
+	 */
+	private static void getTermsInfoFromProperties(Properties properties) {
+		M_log.debug("getTermsInfoFromProperties(): called");
+		String termCount = properties.getProperty(TERM_COUNT);
+		if(termCount.isEmpty()) {
+			M_log.error("The property \"term.count\" is not set in the properties file \"canvasCourseDelete.properties\"");
+			System.exit(1);
+		}
+		int termCountInt = Integer.parseInt(termCount);
+		for(int i=1;i<=termCountInt;i++) {
+			String term = TERM+String.valueOf(i);
+			String termProperty = properties.getProperty(term);
+			if(Utils.isEmpty(termProperty)) {
+				M_log.error("The property \""+term+"\" don't seems to exist in \"canvasCourseDelete.properties\" "
+						+ "please make sure all term properties are numerically aligned and number of properties is equal to \"term.count\" property value");
+				System.exit(1);
+			}
+			String[] termWithDate = termProperty.split(";");
+			DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy");
+			termsInfo.put(termWithDate[0],dtf.parseDateTime(termWithDate[1]));
+		}
+
 	}
      /*
       * This function decide if the api call that need to be made is 'CanvasDirect' or
@@ -93,8 +136,9 @@ public class CourseDelete {
 	/*
 	 *Get the enrollment_terms that are available in canvas during that point of the time.
 	 */
-	private static void getTerms(ApiCallHandler apiHandler, CoursesForDelete cfd) {
-	    M_log.debug("getTerms(): called");
+	private static ArrayList<Term> getTerms(ApiCallHandler apiHandler) {
+		M_log.debug("getTerms(): called");
+		ArrayList<Term> enrollmentTerms = new ArrayList<Term>();
 		HttpResponse httpResponse = apiHandler.getApiResponse(RequestTypeEnum.TERM, null,null);
 		ObjectMapper mapper = new ObjectMapper();
 		int statusCode = httpResponse.getStatusLine().getStatusCode();
@@ -109,14 +153,14 @@ public class CourseDelete {
 			terms = mapper.readValue(jsonResponseString,new TypeReference<HashMap<String,Object>>(){});
 			ArrayList<HashMap<String, Object>> termsList = (ArrayList<HashMap<String, Object>>) terms.get("enrollment_terms");
 			for (HashMap<String, Object> eachTerm : termsList) {
-					String startDate = (String)eachTerm.get("start_at");
-					String endDate = (String)eachTerm.get("end_at");
-					if(startDate!=null&&endDate!=null) {
-						Term term =new Term()
-						.setCanvasTermId(String.valueOf((Integer)eachTerm.get("id")))
-						.setSisTermId((String)eachTerm.get("sis_term_id"))
-						.setTermName((String)eachTerm.get("name"));
-						cfd.addTerm(term);
+				String startDate = (String)eachTerm.get("start_at");
+				String endDate = (String)eachTerm.get("end_at");
+				if(startDate!=null&&endDate!=null) {
+					Term term =new Term()
+							.setCanvasTermId(String.valueOf((Integer)eachTerm.get("id")))
+							.setSisTermId((String)eachTerm.get("sis_term_id"))
+							.setTermName((String)eachTerm.get("name"));
+					enrollmentTerms.add(term);
 				} 
 			}
 		} catch (JsonParseException e) {
@@ -128,16 +172,38 @@ public class CourseDelete {
 		}catch (ParseException e) {
 			M_log.error("getPreviousTerms() has ParseException",e);
 		}
+		return enrollmentTerms;
+	}
+	/*
+	 * This method returns the List of terms for deletion of unused and unpublished courses. 
+	 */
+	private static ArrayList<String> getPreviousTerms(ArrayList<Term> terms) {
+		M_log.debug("getPreviousTerms(): called");
+		DateTime todaysDate = new DateTime();
+		M_log.info("Date the cron job ran: "+todaysDate);
+		ArrayList<String> listOftermsForCoursesDelete = new ArrayList<String>();
+		for (Entry<String, DateTime> term : termsInfo.entrySet()) {
+			if(term.getValue().isBefore(todaysDate)) {
+				String canvasTermIdForSisTermId = getCanvasTermIdForSisTermId(terms, term.getKey());
+				if(canvasTermIdForSisTermId==null) {
+					M_log.error("Cannot find \"CanvasTermId\" for \"sisTermId = "+term.getKey()+
+							"\" provided. Please correct the \"sis.term.id\" property provided from \"canvasCourseDelete.properties\" file.");
+					continue;
+				}
+				listOftermsForCoursesDelete.add(canvasTermIdForSisTermId);
+			}
+
+		}
+		return listOftermsForCoursesDelete;
 	}
     /*
      * For Provided sis_term_id from the properties file 'canvasCourseDelete.properties' we try to
      * get corresponding canvasTermId for making further APi call
      */
-	private static String getCanvasTermIdForSisTermId(CoursesForDelete courseForDelete) {
+	private static String getCanvasTermIdForSisTermId(ArrayList<Term> terms, String termSisId) {
 		M_log.debug("getCanvasTermIdForSisTermId(): called");
-		ArrayList<Term> terms = courseForDelete.getTerms();
 		for (Term term : terms) {
-			if (term.getSisTermId().equalsIgnoreCase(sisTermIdFromPropsFile)) {
+			if (term.getSisTermId().equalsIgnoreCase(termSisId)) {
 				M_log.info("Found canvasTermId for provided SISTermId for the Term:  "+ term.getTermName());
 				return term.getCanvasTermId();
 			}
--- src/main/java/edu/umich/tl/CoursesForDelete.java
+++ src/main/java/edu/umich/tl/CoursesForDelete.java
@@ -5,21 +5,7 @@ import java.util.ArrayList;
 
 
 public class CoursesForDelete {
-	public ArrayList<Term> terms=new ArrayList<Term>();
 	public ArrayList<Course> courses=new ArrayList<Course>();
-	
-
-	public ArrayList<Term> getTerms() {
-		return terms;
-	}
-
-	public void setTerms(ArrayList<Term> terms) {
-		this.terms = terms;
-	}
-	
-	public void addTerm(Term term) {
-		this.terms.add(term);
-	}
 
 	public ArrayList<Course> getCourses() {
 		return courses;
@@ -28,7 +14,7 @@ public class CoursesForDelete {
 	public void setCourses(ArrayList<Course> courses) {
 		this.courses = courses;
 	}
-	
+
 	public void addCourse(Course course) {
 		this.courses.add(course);
 	}
