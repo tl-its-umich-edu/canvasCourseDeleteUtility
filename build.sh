90e1eb3dffde76cd970d51e974e82546b3711275
pushyami <pushyami@umich.edu>
2015-10-21 11:21:36 -0400

    TLUNIZIN-791 Add the more version info ro build

--- build.sh
+++ build.sh
@@ -5,24 +5,18 @@
 # -e: the shell shall immediately exit in case of failure of list of commands
 set -xe
 timestamp=$(date +%Y%m%d%H%M%S)
+
 cd target
 
-# Removing original-canvasCourseDeleteUtility.jar as this is redundant and created as part of <maven-shade-plugin> when creating a uber/fat jar
-#rm original*.jar
-#filename=$(ls *.jar | head -1)
-#targetFilename=$(basename ${filename} .jar)
 #GIT_BRANCH =origin/TLUNIZIN-424 or origin/master jenkins environmental variable to get git branch
-#branch=${GIT_BRANCH}
-#if [ -n "$branch" ]; then
-#btemp=$(basename ${branch} /)
-#else
-#btemp="local"
-#fi
-#mv ${targetFilename}.jar ${targetFilename}.${btemp}.${timestamp}.jar
+branch=${GIT_BRANCH}
+if [ -n "$branch" ]; then
+btemp=$(basename ${branch} /)
+else
+btemp="local"
+fi
 
-echo "shell env variable BUILD_NUMBER is ${BUILD_NUMBER}"
-build_number=${BUILD_NUMBER:=1}
-
+# creating artifact folder
 if [ ! -d "artifact" ]; then
     ## no artifact folder
     ## create such folder first
@@ -30,10 +24,11 @@ if [ ! -d "artifact" ]; then
     mkdir artifact
     chmod -R 755 artifact
 fi
-echo "Generating zip file for build number: ${build_number}"
 
 gitNum=`git log -n 1 --pretty="format:%h"`
-printf 'github version number is %s.' $gitNum > git_version.txt
+
+echo "$JOB_NAME | Build: $BUILD_NUMBER | $GIT_URL | $gitNum | $btemp | $BUILD_ID" > version.txt
+
 
 ## construct new tar file
-#find . -maxdepth 2 -type f \( -name "*_version.txt" -o -name "canvasCourseDeleteUtility.jar" \) -exec tar -rf ./artifact/CCDU_$build_number_$gitNum.tar {} \;
+find . -maxdepth 2 -type f \( -name "version.txt" -o -name "canvasCourseDeleteUtility.jar" \) -exec tar -rf ./artifact/CCDU_${btemp}_${gitNum}_${timestamp}.tar {} \;
